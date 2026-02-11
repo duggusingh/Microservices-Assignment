21c6aa8d7498f12a6f923979bf4b76d052027678
Durgesh Singh <50228320+duggusingh@users.noreply.github.com>
2026-02-11 20:59:05 +0530

    Update order.service.ts

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Order } from '../models/order.model';
 import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class OrderService {
-  constructor(private http: HttpClient) {}
+  private apiUrl = environment.orderApiUrl;
+  constructor(private http: HttpClient) { }
 
-  placeOrder(order: any) {
-    return this.http.post(environment.orderApiUrl, order);
+  placeOrder(order: Order): Observable<any> {
+    return this.http.post(this.apiUrl, order);
   }
 }
