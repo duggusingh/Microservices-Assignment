705dc2791482b79db055852f60982e92affbfa4e
Durgesh Singh <50228320+duggusingh@users.noreply.github.com>
2026-02-11 19:15:02 +0530

    Add ProductListComponent to display products

---
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html'
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAll().subscribe(data => this.products = data);
+  }
+}
