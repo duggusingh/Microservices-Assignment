67ee80e71ea74c72c2c861ecddbcf2384189d0ef
Durgesh Singh <50228320+duggusingh@users.noreply.github.com>
2026-02-11 18:57:29 +0530

    Initialize order service with Serilog and Swagger

---
+++ backend/order-service/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using OrderService.Services;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<OrderDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpClient<IOrderService, OrderService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
