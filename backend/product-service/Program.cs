44a9c396bd94da211769a868e40f9f7d569b9d21
Durgesh Singh <50228320+duggusingh@users.noreply.github.com>
2026-02-11 18:51:32 +0530

    Refactor Serilog and Swagger configuration
    Refactor Serilog configuration and Swagger setup for development environment.

--- backend/product-service/Program.cs
+++ backend/product-service/Program.cs
@@ -1,32 +1,34 @@
-using Serilog;
 using Microsoft.EntityFrameworkCore;
-using Azure.Storage.Blobs;
 using ProductService.Data;
 using ProductService.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Serilog
-Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
 builder.Host.UseSerilog();
 
-// Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); 
-
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ProductDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Azure Blob
-builder.Services.AddSingleton(x => new BlobServiceClient(builder.Configuration["AzureBlob:ConnectionString"]));
 builder.Services.AddScoped<IBlobService, BlobService>();
 
 var app = builder.Build();
 
-app.UseSwagger(); 
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
