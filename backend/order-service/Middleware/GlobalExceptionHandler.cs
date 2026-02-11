0cb771dbfb235770eb76db0fb62677d9a7cbb7f2
Durgesh Singh <50228320+duggusingh@users.noreply.github.com>
2026-02-11 19:54:02 +0530

    Create GlobalExceptionHandler.cs

---
+++ backend/order-service/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using System.Text.Json;
+
+namespace OrderService.Middleware
+{
+    public class GlobalExceptionHandler(RequestDelegate next, ILogger<GlobalExceptionHandler> logger)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An unhandled exception occurred.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error from Durgesh Service. Please try again.",
+                Detailed = exception.Message 
+            };
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
